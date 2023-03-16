e68905ac6e13016ed740ed80b8b1527922a4e7a3
Victoria Helm <vhelm@stanford.edu>
2023-03-16 00:38:32 -0700

    added header stuff to pi-esp.c and explored garbage putk

--- project/sw-uart/pi-esp.c
+++ project/sw-uart/pi-esp.c
@@ -1,25 +1,26 @@
+#include "rpi.h"
+#include "spi.h"
+#include "sw-uart.h"
 #include "pi-esp.h"
-    // first check to make sure we have a valid cmd - make sure to malloc it
-    
-    // TODO formulate and send the cmnd pckt
-    
-    // TODO If nbytes is larger than 0 - malloc and formulate the general data pckt hdr
-    // then loop until nbytes = 0, sending 32 bytes or nbytes%32 of data
-    // I think easiest to just sub bytes send from nbytes until it hits 0. 
-    // adjusting the size of hdr pckt accordingly. 
-    // since this is a serial line we dont worry about ordering. 
-    
-    
-    //TODO wait on a timeout for an ack back from the pi.
-    // can use a get32B cmnd and idk a timeout of like 1/2 second to start
-    //
-    // - eventually this would look like us having a fd that we can read from and 
-    // not having to busy wait here. 
-    
-    //TODO if ack recieved then return 1,
-    //If No-ACK or timeout then resend from the top, probably keep retrans counter and 
-    //eventually return -1 if the retrans maxes out. (this could be like a pi disconnected all of a sudden)
-    
+#include "fast-hash32.h"
+
+// need an enum for the instruction codes
+
+// need to figure out how we represent to/from
+
+// will need a way to compute checksums
+
+/*enum { 
+    ESP_CLIENT_INIT         = 0b0001,
+    ESP_SERVER_INIT         = 0b0010,
+    ESP_SEND_DATA           = 0b0011,
+    ESP_WIFI_CONNECT        = 0b0100,
+    ESP_IS_CONNECTED        = 0b0101,
+    ESP_GET_CONNECTED_LIST  = 0b0110,
+    ESP_NOP                 = 0b0111,
+    ESP_ACK                 = 0b1000,
+    ESP_NOACK               = 0b1001,
+};*/
 
 uint8_t send_cmd(sw_uart_t *u, uint8_t cmd, uint8_t to, uint8_t from, const void *bytes, uint32_t nbytes) {
     // first check to make sure we have a valid cmd - make sure to malloc it
@@ -84,4 +85,4 @@ uint8_t send_cmd(sw_uart_t *u, uint8_t cmd, uint8_t to, uint8_t from, const void
     //If No-ACK or timeout then resend from the top, probably keep retrans counter and 
     //eventually return -1 if the retrans maxes out. (this could be like a pi disconnected all of a sudden)
     return 1;    
-}
+
--- project/sw-uart/pi-esp.h
+++ project/sw-uart/pi-esp.h
@@ -44,6 +44,7 @@ typedef struct esp_cmnd_pckt {
     uint32_t size;
     uint8_t _sbz[13];
 } esp_cmnd_pckt_t;
+
 /* Prompt the esp to init itself as a station aka client in its setup
 Note: might not use, might just flash client code to dedicated client esps */
 uint8_t client_init(void);
@@ -63,10 +64,13 @@ uint8_t server_init(void);
     4 bytes --> Size of data being sent (2GB)
     1 bytes --> The actual Cmd (like connect to wifi or whatever)
     4 bytes --> Check sum of all the data
+
     Top 2 bytes of 32 byte packet:
+
     bits:    15 14 13 12 11   10  9 8 7 6 5 4 3 2 1 0
              | nbytes      |isCmd| from  | to    |sbz|
     byte:               31                 30
+
     Remaining bytes 29-0 are either data or the following if a command packet:
     
     byte:    29 28 27 26  25  24 23 22 21 20 ... 0
