88473c9cba99a44fe3656768b884e67f050bc010
Victoria Helm <vhelm@stanford.edu>
2023-03-17 21:03:33 -0700

    updated arduino test for data pkts

--- project/sw-uart/test_esp_datapkts.ino
+++ project/sw-uart/test_esp_datapkts.ino
@@ -8,6 +8,16 @@ SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
 char buf[32];
 int read = 0;
 
+typedef struct esp_pckt {
+    uint16_t
+        _sbz:2,
+        nbytes:5,
+        isCmd:1,
+        esp_From:4,
+        esp_To:4;
+    uint8_t data[30];
+} esp_pckt;
+
 typedef struct esp_cmnd_pckt{
   uint16_t
     _sbz1:2,
@@ -32,39 +42,60 @@ void setup() {
 
 }
 
-// Test that we can send a simple command with no header from pi to esp:
-// send_cmd(&u,ESP_ACK, 0B1010, 0B111, NULL, 0)
+// Test that we can receive a simple command and 1 data packet
+// send_cmd(&u,ESP_ACK, 0B1010, 0B111, data, 30)
 void loop() {
   
-  if (mySerial.available() == 32){
-    Serial.println("got 32 bytes");
+  if (mySerial.available() == 64){
+    Serial.println("got some bytes!\n");
     //Serial.println("seen something at leas");
-    char buff[32];
+    char cmd[32], data[32];
     for (int i =0; i < 32; i++){
-      buff[i] = mySerial.read();
-    }:
+      cmd[i] = mySerial.read();
+    }
+    for (int i =0; i < 32; i++){
+      data[i] = mySerial.read();
+    }
+    delay(100);
     int nbytes = 0;
     for (int i = 0; i < 32; i++) {
-      nbytes += mySerial.write(buff[i]);
+      nbytes += mySerial.write(cmd[i]);
     }
-    esp_cmnd_pckt * pkt = (esp_cmnd_pckt*) buff;
+    for (int i = 0; i < 32; i++) {
+      nbytes += mySerial.write(data[i]);
+    }
+    esp_cmnd_pckt * cmd_pkt = (esp_cmnd_pckt*) cmd;
+    esp_pckt * data_pkt = (esp_pckt*) data;
+    
     Serial.printf("bytes sent %d\n", nbytes);
 
-    if (pkt->isCmd) {
-      Serial.println("isCmnd checks out\n");
+    if (cmd_pkt->isCmd) {
+      Serial.println("CMD: isCmnd checks out\n");
     }
-    if (pkt->cmnd == 0b1000){
-      Serial.println("command checks out\n");
+    if (cmd_pkt->cmnd == 0b1000){
+      Serial.println("CMD: command checks out\n");
     }
-    if (pkt->esp_To == 0b1010){
-      Serial.println("TO checks out\n");
+    if (cmd_pkt->esp_To == 0b1010){
+      Serial.println("CMD: TO checks out\n");
     }
-    if (pkt->esp_From == 0b1111){
-      Serial.println("FROM checks out\n");
+    if (cmd_pkt->esp_From == 0b1111){
+      Serial.println("CMD: FROM checks out\n");
     }
-    if (pkt->cksum == 0xffffffff) {
-      Serial.println("Checksum checks out!\n");
+    if (cmd_pkt->cksum == 0xffffffff) {
+      Serial.println("CMD: Checksum checks out!\n");
     }
-    Serial.println(buff);
+    if (data_pkt->isCmd == 0) {
+      Serial.println("DATA: isCmnd checks out\n");      
+    }
+    if (data_pkt->nbytes == 30) {
+      Serial.println("DATA: nbytes checks out\n");      
+    }
+    if (cmd_pkt->esp_To == 0b1010){
+      Serial.println("DATA: TO checks out\n");
+    }
+    if (cmd_pkt->esp_From == 0b1111){
+      Serial.println("DATA: FROM checks out\n");
+    }
+    //Serial.println();
   }
 }
