0427a9480789bda406ea73ea0dae20ba0ca512b5
Sarah McCarthy <sarahrm@stanford.edu>
2023-03-19 22:09:12 -0700

    The project .ino is much less ugly but still not complete

--- project/sw-uart/arduino-wifi/project_client/project_client.ino
+++ project/sw-uart/arduino-wifi/project_client/project_client.ino
@@ -1,112 +1,9 @@
-// #include <ESP8266WiFi.h>
-// #include <ESP8266mDNS.h>
-
-// WiFiClient client;
-// WiFiServer wifiServer(80);
-
-// #define MAX_NUM_CLIENTS 16
-// // Modeled from: https://forum.arduino.cc/t/connecting-multiple-clients-with-esp8266/636341
-// // WiFiClient *clients[MAX_NUM_CLIENTS] = {NULL};
-// WiFiClient clients[MAX_NUM_CLIENTS];
-// uint8_t id = 0;
-  
-// void setup() {
-//   Serial.begin(115200);
-//   Serial.print("connected to serial\n");
-
-//   // set up being a station
-//   WiFi.mode(WIFI_STA);
-
-//   // connect to wifi 
-//   const char* ssid = "poop";
-//   const char* password = "password";
-
-//   WiFi.begin(ssid, password);             // Connect to the network
-//   Serial.print("Connecting to ");
-//   Serial.print(ssid); Serial.println(" ...");
-
-
-//   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-//     Serial.print(".");
-//     delay(1000);
-//   }
-
-//  Serial.print("Connected to WiFi. IP:");
-//   Serial.println(WiFi.localIP());
-   
-
-//   // set up mdns
-// if (!MDNS.begin("poop")) {             // Start the mDNS responder
-//     Serial.println("Error setting up MDNS responder!");
-//   }
-//   Serial.println("mDNS responder started");
-
-
-// MDNS.addService("poop", "lab", 80); // Announce service on port 80, port # does not matter at this moment.
-
-// // loop to print out all other connected mdns services 
-// int n = MDNS.queryService("poop", "lab"); // Send out query for esp tcp services
-//   Serial.println("mDNS query done");
-//   if (n == 0) {
-//     Serial.println("no services found");
-//   } else {
-//     Serial.print(n);
-//     Serial.println(" service(s) found");
-//     for (int i = 0; i < n; ++i) {
-//       // Print details for each service found
-//       Serial.print(i + 1);
-//       Serial.print(": ");
-//       Serial.print(MDNS.hostname(i));
-//       Serial.print(" (");
-//       Serial.print(MDNS.IP(i));
-//       Serial.print(":");
-//       Serial.print(MDNS.port(i));
-//       Serial.println(")");
-//       // id = MDNS.IP(i)[3] & 0xf;
-//       // clients[id] = WiFiClient(); 
-//       // while(!clients[id].connect(MDNS.IP(i), MDNS.port(i)));
-//     }
-//   }
-//   Serial.println();
-//   }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   MDNS.update();
-
-//   int n = MDNS.queryService("poop", "lab");
-//   if (n > 0){
-//     Serial.println("connecting");
-//     if (!client.connected()){
-//       if(!client.connect(MDNS.IP(0),MDNS.port(0))){
-//         Serial.println("Could not connect");
-//         return;
-//       }
-//     }  
-//   } else {
-//     Serial.println("no server");
-//     return;
-//   }
-//   // writing to the server supposedl
-//   // if(clients[id].available()) {    
-//   //   char c = clients[id].read();
-//   //   Serial.println(c);    
-//   // }
-//   if(client.available()) {    
-//     char c = client.read();
-//     Serial.println(c);    
-//   }
-
-    
-// }
-
 //client code 
-
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
-  WiFiClient client;
-  WiFiServer wifiServer(80);
+WiFiClient client;
+WiFiServer wifiServer(80);
   
 void setup() {
   Serial.begin(115200);
@@ -119,31 +16,32 @@ void setup() {
   const char* ssid = "poop";
   const char* password = "password";
 
-  WiFi.begin(ssid, password);             // Connect to the network
+  // Connect to the network
+  WiFi.begin(ssid, password); 
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
-
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  // Wait for the Wi-Fi to connect
+  while (WiFi.status() != WL_CONNECTED) { 
     Serial.print(".");
     delay(1000);
   }
 
- Serial.print("Connected to WiFi. IP:");
+  Serial.print("Connected to WiFi. IP:");
   Serial.println(WiFi.localIP());
    
 
   // set up mdns
-if (!MDNS.begin("poop")) {             // Start the mDNS responder
+  if (!MDNS.begin("poop")) {  // Start the mDNS responder
     Serial.println("Error setting up MDNS responder!");
   }
   Serial.println("mDNS responder started");
 
 
-MDNS.addService("poop", "lab", 80); // Announce service on port 80, port # does not matter at this moment.
+  MDNS.addService("poop", "lab", 80); // Announce service on port 80, port # does not matter at this moment.
 
-// loop to print out all other connected mdns services 
-int n = MDNS.queryService("poop", "lab"); // Send out query for esp tcp services
+  // loop to print out all other connected mdns services 
+  int n = MDNS.queryService("poop", "lab"); // Send out query for esp tcp services
   Serial.println("mDNS query done");
   if (n == 0) {
     Serial.println("no services found");
@@ -163,28 +61,33 @@ int n = MDNS.queryService("poop", "lab"); // Send out query for esp tcp services
     }
   }
   Serial.println();
-  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-    MDNS.update();
-    //Serial.printf("Your IP addr is: %d\n", client.remoteIP());
+  MDNS.update();
 
-int n = MDNS.queryService("poop", "lab");
-if (n > 0){
-  //Serial.println("connecting");
-  if (!client.connected()){
-    //Serial.println("Client wasn't connected");
-    if(!client.connect(MDNS.IP(0),MDNS.port(0))){
-      Serial.println("Could not connect");
-      return;
-    }
-  }  
- }else return;
-// writing to the server supposedl
-
-    if(client.available()){    
-    char c = client.read();
-    Serial.println(c);    
-    }
+  int n = MDNS.queryService("poop", "lab");
+  if (n > 0) {
+    //Serial.println("connecting");
+    if (!client.connected()) {
+      //Serial.println("Client wasn't connected");
+      if(!client.connect(MDNS.IP(0),MDNS.port(0))){
+        Serial.println("Could not connect");
+        return;
+      }
+    }  
+  } else {
+    return;
+  } 
+  // writing to the server supposedly
+  if(client.available()){
+    for (int i = 0; i < 5; i++) {
+      char c = client.read();
+      Serial.print(c);
+    }   
+    Serial.println(); 
+    //char c = client.read();
+    //Serial.println(c);    
+  }
 }
--- project/sw-uart/arduino-wifi/project_server/project_server.ino
+++ project/sw-uart/arduino-wifi/project_server/project_server.ino
@@ -1,161 +1,31 @@
-// #include <ESP8266WiFi.h>
-// #include <ESP8266mDNS.h>
-// //server configured to listen on port 1001
-// WiFiServer server(1001);
-// WiFiClient client;
-
-// #define MAX_NUM_CLIENTS 16
-// // Modeled from: https://forum.arduino.cc/t/connecting-multiple-clients-with-esp8266/636341
-// //WiFiClient clients[MAX_NUM_CLIENTS] = malloc(size_of(WiFiClient) * MAX_NUM_CLIENTS); 
-// WiFiClient clients[MAX_NUM_CLIENTS]; 
-// //WiFiClient *clients[MAX_NUM_CLIENTS] = {null}; 
-// uint8_t id = 0;
-// IPAddress ip;
-// uint16_t port;
-
-// void setup() {
-//   // set up serial port so its listening
-//   Serial.begin(115200);
-//   Serial.println("starting");
-
-//   // credentials
-//   const char* ssid = "poop";
-//   const char* password = "password";
-
-//   // for now setup access point 
-//   WiFi.mode(WIFI_AP);
-//   Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
-
-//   // now setup a wifi server to listen on a port. 
-//   server.begin();
-//   client = server.available();
-//   // then set up mdns 
-//   if (!MDNS.begin("poop")) {             // Start the mDNS responder for esp8266.local
-//     Serial.println("Error setting up MDNS responder!");
-//   }
-//   Serial.println("mDNS responder started");
-
-//   MDNS.addService("poop", "lab", 1001); // Announce service on port 1001
-
-//   int n = MDNS.queryService("poop", "lab"); // Send out query for services
-//   Serial.println("mDNS query done");
-//   // if (n == 0) {
-//   //   Serial.println("no services found");
-//   // } else {
-//   while (n == 0) {
-//     n = MDNS.queryService("poop", "lab");
-//     Serial.println("no services found");
-//   }
-//     Serial.print(n);
-//     Serial.println(" service(s) found");
-//     /*
-//     for (int i = 0; i < n; ++i) {
-//       // Print details for each service found
-//       Serial.print(i + 1);
-//       Serial.print(": ");
-//       Serial.print(MDNS.hostname(i));
-//       Serial.print(" (");
-//       Serial.print(MDNS.IP(i));
-//       Serial.print(":");
-//       Serial.print(MDNS.port(i));
-//       Serial.println(")");
-//       // Idea: create a new client with this ip addr and add it to the array 
-//       // https://arduino.stackexchange.com/questions/31256/multiple-client-server-over-wifi
-//       // https://stackoverflow.com/questions/60807733/esp8266-how-to-get-a-clients-ip-with-esp8266-library-v2-6-3-running-as-a-webser
-//       // WiFiClient newClient = server.available();
-//       // clients[i] = new WiFiClient(newClient);
-//     }
-//     */
-    
-//     //int n = MDNS.queryService("poop", "lab"); // Send out query for services
-//     for (int i = 0; i < n; i++) {
-//       Serial.print(i + 1);
-//       Serial.print(": ");
-//       Serial.print(MDNS.hostname(i));
-//       Serial.print(" (");
-//       Serial.print(MDNS.IP(i));
-//       Serial.print(":");
-//       Serial.print(MDNS.port(i));
-//       Serial.println(")");
-//       id = MDNS.IP(i)[3] & 0xf;
-//       ip = MDNS.IP(i);
-//       port = MDNS.port(i);
-//       //clients[id] = new WiFiClient(); 
-//       clients[id] = WiFiClient(); 
-//       //while(!clients[id].connect(MDNS.IP(i), MDNS.port(i)));
-//       clients[id].connect(MDNS.IP(i), MDNS.port(i));
-//     }
-
-  
-//   Serial.println();
-
-// }
-
-// void loop() {
-//   MDNS.update();
-//   //Serial.println(clients[id]);
-//   if (clients[id].connected()) {
-//     Serial.println("writing");
-//     clients[id].write("argh");
-//   } else { 
-//     //clients[id].connect(ip, port);
-//     clients[id].write("argh");
-//     //Serial.println("client not connected :(");
-//   }
-      
-//   /*
-//   WiFiClient clientLocal = server.available(); // returns either a false value or a client with data ready to read. 
-//   if(clientLocal) {
-//     Serial.println("added client");
-//     client = clientLocal;
-//   }
-//   if(client){
-//     Serial.println("writing");
-//     client.write("argh");
-//   }
-//   */
-
-
-
-
-// /*if (0) {
-//     Serial.println("wohoo got a connection");
-//     int avail = client.available();
-//     while(client.connected()){
-//       avail = client.available(); 
-//       if(avail> 0){  
-//         char c = client.read();
-//         Serial.print(c);  
-//         client.write('a'); 
-//         }  
-//       }
-//       yield();      
-//     }*/
-// }
-
 // server code 
-
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+
 //server configured to listen on port 1001
 WiFiServer server(1001);
 WiFiClient client1;
 WiFiClient client2;
-
-//#define MAX_NUM_CLIENTS 16
-//WiFiClient clients[MAX_NUM_CLIENTS]; 
-//WiFiClient * clients = (WiFiClient *)malloc(sizeof(WiFiClient) * MAX_NUM_CLIENTS);
-//uint32_t *clients = (uint32_t*)malloc(sizeof(uint32_t) * MAX_NUM_CLIENTS); 
+WiFiClient client3;
+WiFiClient client4;
+WiFiClient client5;
+WiFiClient client6;
+WiFiClient client7;
+WiFiClient client8;
+WiFiClient client9;
+WiFiClient client10;
+WiFiClient client11;
+WiFiClient client12;
+WiFiClient client13;
+WiFiClient client14;
+WiFiClient client15;
 
 void setup() {
-  
   // set up serial port so its listening
-Serial.begin(115200);
-Serial.println("starting");
-//Serial.printf("sizeof(WiFiClient) = %d\n",sizeof(WiFiClient));
-//Serial.println(clients[0]);
+  Serial.begin(115200);
+  Serial.println("starting");
 
-// credentials
+  // credentials
   const char* ssid = "poop";
   const char* password = "password";
 
@@ -163,119 +33,113 @@ Serial.println("starting");
   WiFi.mode(WIFI_AP);
   Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
 
-// now setup a wifi server to listen on a port. 
-server.begin();
-client1 = server.available();
-client2 = server.available();
+  // now setup a wifi server to listen on a port. 
+  server.begin();
   // then set up mdns 
-if (!MDNS.begin("poop")) {             // Start the mDNS responder for esp8266.local
+  if (!MDNS.begin("poop")) { // Start the mDNS responder for esp8266.local
     Serial.println("Error setting up MDNS responder!");
   }
   Serial.println("mDNS responder started");
 
-MDNS.addService("poop", "lab", 1001); // Announce service on port 1001
+  MDNS.addService("poop", "lab", 1001); // Announce service on port 1001
 
-int n = MDNS.queryService("poop", "lab"); // Send out query for services
+  int n = MDNS.queryService("poop", "lab"); // Send out query for services
   Serial.println("mDNS query done");
   if (n == 0) {
     Serial.println("no services found");
   } else {
     Serial.print(n);
     Serial.println(" service(s) found");
-    // for (int i = 0; i < n; ++i) {
-    //   // Print details for each service found
-    //   Serial.print(i + 1);
-    //   Serial.print(": ");
-    //   Serial.print(MDNS.hostname(i));
-    //   Serial.print(" (");
-    //   Serial.print(MDNS.IP(i));
-    //   Serial.print(":");
-    //   Serial.print(MDNS.port(i));
-    //   Serial.println(")");
-    // }
   }
   Serial.println();
-
 }
 
 uint8_t id = 0;
+uint32_t i2 = 0;
+uint32_t i3 = 0;
 void loop() {
-  // int n = 0;
-  // MDNS.update();
-  // WiFiClient clientLocal = server.available(); // returns either a false value or a client with data ready to read. 
-
-  // if (clientLocal) {
-  //   uint8_t id = clientLocal.remoteIP()[3];
-  //   if (!clients[id]) {
-  //     clients[id] = clientLocal; // IDK if this will work. Maybe try 
-  //   }  
-  //   if(clients[id]) {
-  //   Serial.println("writing");
-  //   clients[id].write("argh");
-  //   }
-  // }
-   MDNS.update();
-   //Serial.println("About to check clients[id] in loop");
-  //  WiFiClient client = (WiFiClient)clients[id];
-  //  if (client) {
-  //    Serial.println("writing");
-  //    //WiFiClient client = (WiFiClient)clients[id];
-  //    client.write("argh");
-  //  } else {
-  //    Serial.println("about to server.available() in loop");
-  //    WiFiClient clientLocal = server.available();
-  //    if (clientLocal) {
-  //      Serial.println("Found a client!");       
-  //      id = clientLocal.remoteIP()[3];
-  //      Serial.printf("id: %d\n", id);
-  //      //if (!clients[id]) {
-  //       clients[id] = WiFiClient(clientLocal); // IDK if this will work. Maybe try 
-  //      //} 
-  //    }
-     
-  //  }
-
+  MDNS.update(); // Always have to do in loop
   WiFiClient clientLocal = server.available(); 
   if(clientLocal) {
     Serial.println("added client");
     uint8_t id = clientLocal.remoteIP()[3];
     switch(id){
-      case 3:
+      case 1:
         client1 = clientLocal;
         break;
       case 2:
         client2 = clientLocal;
         break;
+      case 3:
+        client3 = clientLocal;
+        break;
+      case 4:
+        client4 = clientLocal;
+        break;
+      case 5:
+        client5 = clientLocal;
+        break;
+      case 6:
+        client6 = clientLocal;
+        break;
+      case 7:
+        client7 = clientLocal;
+        break;
+      case 8:
+        client8 = clientLocal;
+        break;
+      case 9:
+        client9 = clientLocal;
+        break;
+      case 10:
+        client10 = clientLocal;
+        break;
+      case 11:
+        client11 = clientLocal;
+        break;
+      case 12:
+        client12 = clientLocal;
+        break;
+      case 13:
+        client13 = clientLocal;
+        break;
+      case 14:
+        client14 = clientLocal;
+        break;
+      case 15:
+        client15 = clientLocal;
+        break;  
       default:
+        Serial.println("invalid id?");
         break;
     }
   }
-  if(client1 && client1.connected()){
-    Serial.println("writing 1");
-    client1.write("victoria");
+  if(client1 && client1.connected()) {
+    Serial.println("writing to client1");
+    client1.write("abby!");
   }
-  if(client2 && client2.connected()){
-    Serial.println("writing 2");
-    client2.write("sarah");
+  if(client2 && client2.connected()) {
+    Serial.printf("writing to client2: %d\n", i2++);
+    client2.write("beth!");
   }
+  if(client3 && client3.connected()) {
+    Serial.printf("writing to client3: %d\n", i3++);
+    client3.write("cathy");
+  }
+  if(client4 && client4.connected()) {
+    Serial.println("writing to client4");
+    client3.write("daisy");
+  }
+  if(client5 && client5.connected()) {
+    Serial.println("writing to client5");
+    client3.write("essie");
+  } 
+  if(client6 && client6.connected()) {
+    Serial.println("writing to client6");
+    client3.write("fran!");
+  } 
+  // TODO: Fill in for remaining clients
+  // TODO: Perhaps add this to the switch case
+  // TODO: Figure out why we are writing to clients that aren't connected
 }
 
-// clientLocal.remoteIP()[3]
-// Use IP addr to see if client already there.
-
-
-
-/*if (0) {
-    Serial.println("wohoo got a connection");
-    int avail = client.available();
-    while(client.connected()){
-      avail = client.available(); 
-      if(avail> 0){  
-        char c = client.read();
-        Serial.print(c);  
-        client.write('a'); 
-        }  
-      }
-      yield();      
-    }*/
-//}
